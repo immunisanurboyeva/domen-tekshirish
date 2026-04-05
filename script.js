9bb96a3c008ddcd931826eb075036c34f650101b
Munisa Nurboyeva <nurboyevaa@Munisas-MacBook.local>
2026-04-05 17:32:42 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,48 @@
+const form = document.querySelector('.form')
+const input = document.querySelector('#input')
+const btn = document.querySelector('.btn')
+const text = document.querySelector('.text')
+
+form.addEventListener('submit', (e)=>{
+    e.preventDefault()
+
+    checkDomain(input.value)
+    input.value = ""
+})
+
+async function checkDomain(domain) {
+    try{
+        text.textContent = 'qidirilmoqda...'
+        text.style.color = 'gray'
+
+        const res = await fetch(
+            `https://api.api-ninjas.com/v1/domain?domain=${domain}`,
+            {
+                headers: {
+                    "X-Api-Key": "GnxpyjjORxM1tGWnAHb104SzKiIG8McqnS7opIpX",
+                }
+            }
+        )
+
+        console.log(res);
+        const data = await res.json()
+        console.log(data);
+
+        
+        if(data.available){
+            text.textContent = `${domain} mavjud`
+            text.style.color  = 'rgb(72, 226, 177)'
+        } else{
+            text.textContent = `${domain} mavjud emas`
+            text.style.color  = 'rgb(186, 68, 123)'
+        }
+
+        if(!res.ok){
+            throw new Error(".com formatda kiriting")
+        }
+        
+    } catch (error){
+        text.textContent = error.message
+        text.style.color  = 'rgb(186, 68, 123)'
+    }
+}
